4352df3e9eb507943051fe87497ba11f5a89de52
Brian Astrup Mikkelsen <brian@mikkelsen.com>
2013-06-26 02:58:23 +0200

    replicate, spawns new isolates and adds them to the network

---
+++ lib/replicate.dart
@@ -0,0 +1,31 @@
+/**
+  * receive a list of sendport, 
+  * move first element to end of list, and send the modified list to the first
+  */
+import 'dart:isolate';
+import 'dart:async';
+import 'dart:math';
+
+main(){
+  Random r = new Random();
+  port.receive((msg,reply) {
+      var name = msg['current'];
+
+      // spawn new isolate and add to list
+      var newid  = "${name}_${msg['value']++}";
+      var new_isolate = spawnUri('replicate.dart');
+      msg[newid] = new_isolate;
+
+      // remove current isolate from list
+      msg.remove(name);
+
+      // pick next
+      List keys = msg.keys.toList()..remove('value')..remove('current');
+      String next = keys[r.nextInt(keys.length)];
+      SendPort _port = msg[next];
+      msg['current'] = next;
+
+      print("$name: spawning $newid, next  is: $next");
+      _port.send(msg);
+    });
+}
